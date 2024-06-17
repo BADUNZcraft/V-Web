46f9e3f3be893edce1c66d558946c5c5e21f01df
Kristian <schmaren937@gmail.com>
2024-06-17 15:15:51 +0200

    header

--- program/src/components/Header.jsx
+++ program/src/components/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 export default function Header() {
   return (
-    <div>
-        <h1>Header</h1>
-    </div>
-  )
+    <header>
+      <div className="header">
+        <nav className="navbar">
+          <div className="nav-group">
+            <a href="program\src\components\HomePage.jsx" className="nav-item">Home</a>
+            <a href="program\src\components\Wishlist.jsx" className="nav-item">Wishlist</a>
+          </div>
+          <a href="#" className="nav-item button">Logout</a>
+        </nav>
+      </div>
+    </header>
+  );
 }
