48115148d7978ae3b1fc8ae0965adf4c5c08616b
Loris Rüegg <lorisrueegg@icloud.com>
2024-06-20 08:03:34 +0200

    fix links and login

--- program/src/components/Header.jsx
+++ program/src/components/Header.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAuth } from '../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const [error, setError]= useState('')
+    const {currentUser, logout } = useAuth()
+    const navigate = useNavigate()
+
+    async function handleLogout() {
+        setError('')
+
+        try {
+            await logout()
+            navigate('/login')
+        } catch {
+            console.log('Failed to log out')
+        }
+    }
   return (
     <header>
       <div className="header">
         <nav className="navbar">
           <div className="nav-group">
-            <a href="program\src\components\HomePage.jsx" className="nav-item">Home</a>
-            <a href="program\src\components\Wishlist.jsx" className="nav-item">Wishlist</a>
+            <a href="/" className="nav-item">Home</a>
+            <a href="/wishlist" className="nav-item">Wishlist</a>
           </div>
-          <a href="#" className="nav-item button">Logout</a>
+          <input type='button' onClick={handleLogout} className="nav-item button" value={'Log out'}/>
         </nav>
       </div>
     </header>
