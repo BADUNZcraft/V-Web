e21d5ef6bd969e7ec43a2098c7d3b97ffffc4bc9
Kristian <schmaren937@gmail.com>
2024-06-20 13:42:06 +0200

    gamecard mit like und so

--- program/src/components/GameCard.jsx
+++ program/src/components/GameCard.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function GameCard() {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const toggleHeart = () => {
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div>
-      <div class="game-card">
-       <div class="game-card-image">
-            <img src="https://placehold.co/150x150" alt="#" />
+<div className="game-card">
+        <div className="game-card-image">
+          <img src="https://placehold.co/150x150" alt="#" />
         </div>
-        <div class="game-card-title">
-            <p>Game Name</p>
+        <div className="game-card-title">
+          <p>Game Name</p>
         </div>
-        <div class="game-card-rating">
-            <p>000.00</p>
+        <div className="game-card-info">
+          <div className="game-card-rating">
+            <p>000.00 rating</p>
+          </div>
+          <button onClick={toggleHeart} className="heart-button" style={{ fontSize: '24px' }}>
+            {isLiked ? '❤️' : '🤍'}
+          </button>
         </div>
-        <button>
-
-        </button> 
       </div>
-
-
     </div>
-  )
+  );
 }
